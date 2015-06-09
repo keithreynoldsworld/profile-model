90deeb2474cbb0d69140ef7e9565d394dc2fdc41
Keith Reynolds <keithreynoldsworld@gmail.com>
2015-06-09 15:43:21 -0500

    working

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,18 +1,46 @@
-var user = new UserModel();
-var App = Backbone.Router.extend({
-	routes: {
-		'': 'profile',
-		'edit': 'edit'
-	},
-	profile: function() {
-		$('.page').hide();
-		$('#profile').show();
-	},
-	edit: function() {
-		$('.page').hide();
-		$('#edit').show();
-	}
-});
+$(document).ready(function(){   
 
-var app = new App();
-Backbone.history.start();
+	var user = new UserModel();
+	var App = Backbone.Router.extend({
+		routes: {
+			'': 'profile',
+			'home':'profile',
+			'edit': 'edit'
+		},
+		profile: function() {
+			$('.page').hide();
+			$('#profile').show();
+		},
+		edit: function() {
+			$('.page').hide();
+			$('#edit').show();
+		}
+	});
+
+	var app = new App();
+	Backbone.history.start();	
+
+	var user = new UserModel();
+
+	user.on('change', update);
+
+
+	function update(UserModel){
+		$('.profile-usertitle-name').html(UserModel.get('name'));
+		$('.profile-usertitle-job').html(UserModel.get('email'));
+		$('.dropdown-toggle').html(UserModel.get('name'));
+	}
+
+	function fillUser(e){
+		e.preventDefault();
+		user.set({
+			name: $('#name').val(),
+			role: $('#role').val(),
+			email: $('#inputEmail3').val(),
+		});
+		
+	}
+
+	$('form').on('submit', fillUser);
+
+});
